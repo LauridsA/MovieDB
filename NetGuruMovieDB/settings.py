4583d8ffbc3063f13c986762f8e9881a03e5d341
LauridsA <lauridsandersen2013@gmail.com>
2019-05-04 15:55:46 +0200

    package name ...

--- NetGuruMovieDB/settings.py
+++ NetGuruMovieDB/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    '.apps.NetGuruMovieDB'
+    'NetGuruMovieDB.apps.NetGuruMovieDB',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
